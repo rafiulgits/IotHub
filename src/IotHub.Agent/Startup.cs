1ecc56f32f7f975b8cbbe615e84be19bf7776cf5
Rafiul Islam <avoidcloud@gmail.com>
2020-05-16 15:56:36 +0600

    agent mqtt settings read from appSettings file

--- src/IotHub.Agent/Startup.cs
+++ src/IotHub.Agent/Startup.cs
@@ -1,6 +1,7 @@
 using IotHub.Agent.Configurations;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -8,11 +9,17 @@ namespace IotHub.Agent
 {
     public class Startup
     {
+        private IConfiguration configuration;
+        public Startup(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddHostedMqttClient();
+            services.AddHostedMqttClient(configuration);
             services.AddConfiuredCors();
             services.AddSignalR();
         }
